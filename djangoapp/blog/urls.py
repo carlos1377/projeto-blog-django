76451524015978f1e1eab62969c6f773c8e02821
carlos1377 <carloseduardoorso1@gmail.com>
2024-02-29 16:40:50 -0300

    created and updated templates and css for the base of the blog

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.conf import settings
-from blog.views import index
+from blog import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', views.index, name='index'),
+    path('post/', views.post, name='post'),
+    path('page/', views.page, name='page'),
 ]
